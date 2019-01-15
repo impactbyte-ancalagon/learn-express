fcfc81508a24572a30984d892a46802bc8319b3a
M Haidar Hanif <git@mhaidarhanif.com>
2019-01-15 16:16:45 +0700

    Use CORS

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const cors = require("cors")
 
 const app = express()
 const port = 8000
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded())
 
