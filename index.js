4640e97247105cd70e50bdee192c1f33eeb4b865
M Haidar Hanif <git@mhaidarhanif.com>
2019-01-15 16:09:55 +0700

    Include Postman collection

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const bodyParser = require("body-parser")
 
 const app = express()
-const port = 3000
+const port = 8000
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded())
@@ -44,7 +44,8 @@ let furnitures = {
 
 app.get("/", (req, res) => {
   res.send({
-    message: "Hello World"
+    message: "Hello World",
+    headers: req.headers
   })
 })
 
